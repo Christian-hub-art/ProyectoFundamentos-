6d6cc4f430345b9abd71d8caad40e8daa674326d
Christian-hub-art <christianbecerra@javeriana.edu.co>
2025-11-29 16:27:58 +0000

    .

--- GestorVehiculosSpring/src/main/java/com/example/gestorvehiculos/repository/VehiculoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gestorvehiculos.repository;
-
-import com.example.gestorvehiculos.model.Vehiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {}
